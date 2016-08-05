b83088452aff00773141909dc9e3544971a3bb6d
yulingsong <yulingsong5@126.com>
2016-08-05 16:20:50 +0800

    Initial Commit

---
+++ TouchIDSimpleDemoOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchIDSimpleDemoOne
+//
+//  Created by yulingsong on 16/8/5.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchIDSimpleDemoOne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TouchIDSimpleDemoOne
+//
+//  Created by yulingsong on 16/8/5.
+//  Copyright © 2016年 yulingsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
